c255a325d934ec19603ece97c862fd87b9c2ae24
juo2 <849616969@qq.com>
2024-03-23 17:14:52 +0800

    删除微信框架 测试vue

--- Assets/RuntimeGraph/Scripts/ConnectVue.cs
+++ Assets/RuntimeGraph/Scripts/ConnectVue.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ConnectVue : MonoBehaviour
 {
     [DllImport("__Internal")]
     private static extern void PostScore(string currentScene);  //µ±Ç°³¡¾°
+
+    public Text label;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +17,12 @@ public class ConnectVue : MonoBehaviour
         Debug.Log("12345678");
         PostScore("12345678");
     }
+
+    public void OnCallBack(string msg)
+    {
+        label.text = msg;
+    }
+
 
     // Update is called once per frame
     void Update()
