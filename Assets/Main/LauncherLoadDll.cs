ddd61449143b98bd05465417616e35041dcfec55
梁嘉齐 <mikiliang@kugou.net>
2024-04-12 18:12:23 +0800

    尝试提交 热更新模板

---
+++ Assets/Main/LauncherLoadDll.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using HybridCLR;
+using System.Collections;
+using System;
+using System.Linq;
+using UnityEngine.Networking;
+using System.Reflection;
+
+public class LauncherLoadDll : MonoBehaviour
+{
+    private static Dictionary<string, byte[]> s_assetDatas = new Dictionary<string, byte[]>();
+    private static Assembly _hotUpdateAss;
+
+    private static List<string> AOTMetaAssemblyFiles { get; } = new List<string>()
+    {
+        "mscorlib.dll.bytes",
+        "System.dll.bytes",
+        "System.Core.dll.bytes",
+    };
+
+    void Start()
+    {
+        StartCoroutine(DownLoadAssets(this.StartGame));
+    }
+
+
+    public static byte[] ReadBytesFromStreamingAssets(string dllName)
+    {
+        return s_assetDatas[dllName];
+    }
+
+    private string GetWebRequestPath(string asset)
+    {
+        var path = $"{Application.streamingAssetsPath}/{asset}";
+        if (!path.Contains("://"))
+        {
+            path = "file://" + path;
+        }
+        return path;
+    }
+
+    IEnumerator DownLoadAssets(Action onDownloadComplete)
+    {
+        var assets = new List<string>
+        {
+            "HotUpdate.dll.bytes",
+        }.Concat(AOTMetaAssemblyFiles);
+
+        foreach (var asset in assets)
+        {
+            string dllPath = GetWebRequestPath(asset);
+            Debug.Log($"start download asset:{dllPath}");
+            UnityWebRequest www = UnityWebRequest.Get(dllPath);
+            yield return www.SendWebRequest();
+
+#if UNITY_2020_1_OR_NEWER
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+#else
+            if (www.isHttpError || www.isNetworkError)
+            {
+                Debug.Log(www.error);
+            }
+#endif
+            else
+            {
+                // Or retrieve results as binary data
+                byte[] assetData = www.downloadHandler.data;
+                Debug.Log($"dll:{asset}  size:{assetData.Length}");
+                s_assetDatas[asset] = assetData;
+            }
+        }
+
+        onDownloadComplete();
+    }
+
+    private static void LoadMetadataForAOTAssemblies()
+    {
+        /// 注意，补充元数据是给AOT dll补充元数据，而不是给热更新dll补充元数据。
+        /// 热更新dll不缺元数据，不需要补充，如果调用LoadMetadataForAOTAssembly会返回错误
+        /// 
+        HomologousImageMode mode = HomologousImageMode.SuperSet;
+        foreach (var aotDllName in AOTMetaAssemblyFiles)
+        {
+            byte[] dllBytes = ReadBytesFromStreamingAssets(aotDllName);
+            // 加载assembly对应的dll，会自动为它hook。一旦aot泛型函数的native函数不存在，用解释器版本代码
+            LoadImageErrorCode err = RuntimeApi.LoadMetadataForAOTAssembly(dllBytes, mode);
+            Debug.Log($"LoadMetadataForAOTAssembly:{aotDllName}. mode:{mode} ret:{err}");
+        }
+    }
+
+    void StartGame()
+    {        LoadMetadataForAOTAssemblies();
+#if !UNITY_EDITOR
+        _hotUpdateAss = Assembly.Load(ReadBytesFromStreamingAssets("HotUpdate.dll.bytes"));
+#else
+        _hotUpdateAss = System.AppDomain.CurrentDomain.GetAssemblies().First(a => a.GetName().Name == "HotUpdate");
+#endif
+
+        System.Type type = _hotUpdateAss.GetType("Launcher");
+        GameObject xgame = new GameObject("xgame", type);
+        DontDestroyOnLoad(xgame);
+    }
+
+}
--- Assets/Plugins/AssetProcessor/Editor/QuickMenuKey.cs
+++ Assets/Plugins/AssetProcessor/Editor/QuickMenuKey.cs
@@ -117,7 +117,7 @@ public class QuickMenuKey : ScriptableObject
         UnityEngine.SceneManagement.SceneManager.CreateScene("LaunchGame");
         UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(scene);
 
-        var _hotUpdateAss = System.AppDomain.CurrentDomain.GetAssemblies().First(a => a.GetName().Name == "Main");
+        var _hotUpdateAss = System.AppDomain.CurrentDomain.GetAssemblies().First(a => a.GetName().Name == "HotUpdate");
         System.Type type = _hotUpdateAss.GetType("Launcher");
 
         //System.Reflection.Assembly Assembly = System.Reflection.Assembly.Load("Assembly-CSharp");
